b7976a8ce7fd324fa14864dadd4629c8156fdde7
Gotharo <gonzalo.castro@gotharo.cl>
2025-07-14 12:47:34 -0400

    first commit

---
+++ assets/js/scripth.js
@@ -0,0 +1,6 @@
+let input = document.querySelector("#input");
+let select = document.querySelector("#select");
+let btnBuscar = document.querySelector("#btnBuscar");
+let clpFinal = document.querySelector("#monto-final");
+
+
